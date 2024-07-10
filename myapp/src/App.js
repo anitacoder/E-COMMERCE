4e1b6490312dbab9c9145b49be6a4bd5688aa8cc
Anita <writeanita.n@gmail.com>
2024-07-10 23:38:27 +0100

    Implemented routing in App component using react-router-dom library. Added routes for Home, Privacy, LoginForm, Contact, and SignUp components.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
-
-function App() {
-  return <div>App</div>
-}
+import Contact from "./Components/contact";
+import Home from "./Components/Home";
+import LoginForm from "./Components/loginForm";
+import Privacy from "./Components/privacy";
+import SignUp from "./Components/signUp";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+const App = () =>  {
+  return <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/privacy" element={<Privacy/>}/>  
+        <Route path="/LoginForm" element={<LoginForm/>}/>  
+        <Route path="/Contact" element={<SignUp/>}/>  
+        <Route path="/SignUp" element={<Contact/>}/>    
+      </Routes>
+    </Router>
+  </div>
+};
 
 export default App;
