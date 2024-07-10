edba3381beba0d0ea9aa6418cbfe4343e00438ba
Anita <writeanita.n@gmail.com>
2024-07-11 00:14:25 +0100

    Implemented a navbar layout with a search input field.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,16 +4,18 @@ import Home from "./Components/Home";
 import LoginForm from "./Components/loginForm";
 import Privacy from "./Components/privacy";
 import SignUp from "./Components/signUp";
+import Navbar from "./Components/Navbar";
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 const App = () =>  {
   return <div>
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/privacy" element={<Privacy/>}/>  
         <Route path="/LoginForm" element={<LoginForm/>}/>  
-        <Route path="/Contact" element={<SignUp/>}/>  
-        <Route path="/SignUp" element={<Contact/>}/>    
+        <Route path="/Contact" element={<Contact/>}/>  
+        <Route path="/SignUp" element={<SignUp/>}/>    
       </Routes>
     </Router>
   </div>
