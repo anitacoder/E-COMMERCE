0e6b099919841af129f723823c5bca1f1a8fa917
Anita <writeanita.n@gmail.com>
2024-07-11 23:09:12 +0100

    Added an h1 tag, p tag and a button to Home.jsx

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Home = () => {
     return(
-        <div>Home</div>
+        <div className="home_header">
+            <div className="header_container">
+                <h1>Hairs</h1>
+                <p>Start from the top. We've got you covered with a range of formulas to keep your hair in tip-top shape and your style in mint condition</p>
+                <button>Shop Shampoo</button>
+            </div>
+        </div>
     )
 }
 
