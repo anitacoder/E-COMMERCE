7b9838e7879521373f0eee8b2b2677941594a156
Anita <writeanita.n@gmail.com>
2024-07-11 23:15:53 +0100

    Created a file called Home.css

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import "./Home.css";
+import imageHeader from "./Image.huron.jpg"
 
 const Home = () => {
     return(
         <div className="home_header">
+            <div className="data">
             <div className="header_container">
                 <h1>Hairs</h1>
                 <p>Start from the top. We've got you covered with a range of formulas to keep your hair in tip-top shape and your style in mint condition</p>
-                <button>Shop Shampoo</button>
+                <button className="shampoobtn">Shop Shampoo</button>
             </div>
+        </div>
         </div>
     )
 }
