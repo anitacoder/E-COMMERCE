3f8728d4074b941572163760c7c6d3d396b88513
Anita <writeanita.n@gmail.com>
2024-07-11 17:57:15 +0100

    Deleted and reinstalled package-lock.json and also created a file in the component called App.css

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import refreshlogos from "./Images/refreshlogo.png";
 import "./Navbar.css";
 import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     return(
@@ -17,15 +18,15 @@ const Navbar = () => {
                     <div className="navbar_main">
                         <div>
                             <div>
-                                {/* search icon */}
+                            <SearchIcon className="search_icon"/>
                             </div>
-                             {/* Menu icon */}
+                            <MenuIcon className="menu_icon"/>
                         </div>
                         <div>
                             <img src={refreshlogos} className="heading_logo" alt=""/>
                         </div>
                         <div className="badSearch">
-                             {/* search icon */}
+                        <SearchIcon className="search_icon"/>
                              {/* local icon */}  
                         </div>
                     </div>
