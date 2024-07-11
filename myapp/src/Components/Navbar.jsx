7912fc25c4e32c7a23abef8942ffdfcba48df14a
Anita <writeanita.n@gmail.com>
2024-07-11 18:58:26 +0100

    Added an even listener to the menu icon and search icon

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -7,29 +7,41 @@ import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
+import { useState } from "react";
 
 const Navbar = () => {
+    const [data, setdata] = useState(false);
+    const [menu, setMenu] = useState(false);
+    
+    const inputHandler = () => {
+        setdata(!data);
+    };
+
+    const menuHandler = () => {
+        setMenu(!data)
+    }
     return(
         <div>
             <div className="navbar_div">
                 <div>
                     <input type="text" placeholder="Search here" className="search"/>
                     <SearchIcon className="search_icon"/>
-                   <CloseIcon class="close_icon"/>
+                   <CloseIcon onClick={inputHandler}class="close_icon"/>
                 </div>
                 <div>
                     <div className="navbar_main">
+                        <div onClick={menuHandler}/>
                         <div>
                             <div>
                             <SearchIcon className="search_icon"/>
                             </div>
-                            <MenuIcon className="menu_icon"/>
+                            <MenuIcon onClick={inputHandler}className="menu_icon"/>
                         </div>
                         <div>
                             <img src={refreshlogos} className="heading_logo" alt=""/>
                         </div>
                         <div className="badSearch">
-                        <SearchIcon className="search_icon"/>
+                        <SearchIcon onClick={inputHandler}className="search_icon"/>
                         <LocalMallIcon className="localMall_icon"/>
                         </div>
                     </div>
