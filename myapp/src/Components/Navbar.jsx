d5f9ed25c41d0979f6b7c85257a221109752c54b
Anita <writeanita.n@gmail.com>
2024-07-11 19:24:23 +0100

    Added an even listener to the close icon

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import refreshlogos from "./Images/refreshlogo.png";
 import "./Navbar.css";
@@ -7,42 +7,39 @@ import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
-import { useState } from "react";
 
 const Navbar = () => {
-    const [data, setdata] = useState(false);
+    const [data, setData] = useState(false);
     const [menu, setMenu] = useState(false);
     
     const inputHandler = () => {
-        setdata(!data);
+        setData(!data);
     };
 
     const menuHandler = () => {
-        setMenu(!data)
+        setMenu(!menu);
     }
-    return(
+
+    return (
         <div>
             <div className="navbar_div">
-                <div>
-                    <input type="text" placeholder="Search here" className="search"/>
-                    <SearchIcon className="search_icon"/>
-                   <CloseIcon onClick={inputHandler}className="close_icon"/>
+                <div className={data ? "input_form" : "input_form_not"}>
+                    <input type="text" placeholder="Search here" className="search" />
+                    <SearchIcon className="search_icon" />
+                    <CloseIcon onClick={() => setData(false)} className="close_icon" />
                 </div>
                 <div>
                     <div className="navbar_main">
-                        <div onClick={menuHandler}/>
                         <div>
-                            <div>
-                            <SearchIcon className="search_icon"/>
-                            </div>
-                            <MenuIcon onClick={inputHandler}className="menu_icon"/>
+                            <SearchIcon onClick={inputHandler} className="search_icon" />
+                            <MenuIcon onClick={menuHandler} className="menu_icon" />
                         </div>
                         <div>
-                            <img src={refreshlogos} className="heading_logo" alt=""/>
+                            <img src={refreshlogos} className="heading_logo" alt="" />
                         </div>
                         <div className="badSearch">
-                        <SearchIcon onClick={inputHandler}className="search_icon"/>
-                        <LocalMallIcon className="localMall_icon"/>
+                            <SearchIcon onClick={inputHandler} className="search_icon" />
+                            <LocalMallIcon className="localMall_icon" />
                         </div>
                     </div>
                 </div>
@@ -52,16 +49,15 @@ const Navbar = () => {
             <Link to="/privacy" className="link">Privacy</Link>
             <Link to="loginForm" className="link">Login</Link>
             <Link to="/signUp" className="link">SignUp</Link>
-          <div>
-          <Link to="/" className="sidebar_link">Home</Link>
-          <Link to="/contact" className="sidebar_link">Contact</Link>
-          <Link to="/privacy" className="sidebar_link">Privacy</Link>
-          <Link to="loginForm" className="sidebar_link">Login</Link>
-          <Link to="/signUp" className="lsidebar_link">SignUp</Link>
-      </div>
+            <div>
+                <Link to="/" className="sidebar_link">Home</Link>
+                <Link to="/contact" className="sidebar_link">Contact</Link>
+                <Link to="/privacy" className="sidebar_link">Privacy</Link>
+                <Link to="loginForm" className="sidebar_link">Login</Link>
+                <Link to="/signUp" className="sidebar_link">SignUp</Link>
+            </div>
         </div>
-       
-    )
+    );
 }
 
 export default Navbar;
