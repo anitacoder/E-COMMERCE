31ae359ca39d857b6a4c1c14c7b77276d7494088
Anita <writeanita.n@gmail.com>
2024-07-11 22:23:07 +0100

    Added the sidebar links to be a dropdown link of the menu bar

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -9,53 +9,47 @@ import LocalMallIcon from '@mui/icons-material/LocalMall';
 
 const Navbar = () => {
     const [data, setData] = useState(false);
-    const [menu, setMenu] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     
     const inputHandler = () => {
         setData(!data);
     };
 
     const menuHandler = () => {
-        setMenu(!menu);
+        setMenuOpen(!menuOpen);
     }
 
     return (
         <div>
             <div className="navbar_div">
-                <div className={data ? "input_form" : "input_form_not"}>
+                <div>
                     <SearchIcon className="search_icon_inside" />
                     <input type="text" placeholder="Search here" className="search" />
-                    <CloseIcon className="close_icon" />
+                    <CloseIcon onClick={inputHandler} className="close_icon" />
                 </div>
                 <div>
                     <div className="navbar_main">
-                        <div onClick={inputHandler} />
+                        <div>
+                        </div>
                         <div>
                             <img src={refreshlogos} className="heading_logo" alt="" />
                         </div>
                         <div className="badSearch">
                             <LocalMallIcon className="localMall_icon" />
-                        </div>
-                        <div>
                             <MenuIcon onClick={menuHandler} className="menu_icon" />
                         </div>
-                    </div>
                 </div>
             </div>
-            <Link to="/" className="link">Home</Link>
-            <Link to="/contact" className="link">Contact</Link>
-            <Link to="/privacy" className="link">Privacy</Link>
-            <Link to="loginForm" className="link">Login</Link>
-            <Link to="/signUp" className="link">SignUp</Link>
-            <div>
-                <Link to="/" className="sidebar_link">Home</Link>
-                <Link to="/contact" className="sidebar_link">Contact</Link>
-                <Link to="/privacy" className="sidebar_link">Privacy</Link>
-                <Link to="loginForm" className="sidebar_link">Login</Link>
-                <Link to="/signUp" className="sidebar_link">SignUp</Link>
-            </div>
+            {menuOpen && (
+                <div className="sidebar">
+                    <Link to="/" className="sidebar_link">Home</Link>
+                    <Link to="/contact" className="sidebar_link">Contact</Link>
+                    <Link to="/privacy" className="sidebar_link">Privacy</Link>
+                    <Link to="loginForm" className="sidebar_link">Login</Link>
+                    <Link to="/signUp" className="sidebar_link">SignUp</Link>
+                </div>
+            )}
         </div>
     );
-}
-
+};
 export default Navbar;
