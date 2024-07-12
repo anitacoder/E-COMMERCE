3187f7c271c94cecb586ef2ca857027b555d0a66
Anita <writeanita.n@gmail.com>
2024-07-12 13:45:02 +0100

    Added LoginForm component with email, password inputs, and login button

--- myapp/src/Components/loginForm.jsx
+++ myapp/src/Components/loginForm.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import "./LoginForm.css"; 
 
 const LoginForm = () => {
-    return(
-        <div>Login</div>
+    return (
+        <div className="login-container">
+            <h1>Login</h1>
+            <div className="login-content">
+                <input type="email" placeholder="Email" className="login-input" />
+                <input type="password" placeholder="Password" className="login-input" />
+                <button className="login-button">Login</button>
+            </div>  
+        </div>
     )
 }
 
