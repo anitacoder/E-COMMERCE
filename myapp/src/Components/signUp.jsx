b842becbecabe8b5c0527e7c27c9911c1045fa37
Anita <writeanita.n@gmail.com>
2024-07-12 15:41:48 +0100

    Added SignUp component with input fields and login button

--- myapp/src/Components/signUp.jsx
+++ myapp/src/Components/signUp.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const SignUp = () => {
   return (
-    <div>Sign Up</div>
-  );
+    <div>
+        <div className="signIn-content">
+            <h1>Sign in</h1>
+            <input type="name" placeholder="Name" className="sign-input" />
+            <input type="surname" placeholder="surname" className="sign-input" />
+            <input type="email" placeholder="Email" className="sign-input" />
+            <input type="password" placeholder="Password" className="sign-input" />
+            <button className="login-button">Login</button>
+        </div>
+    </div>
+);
 };
 
 export default SignUp;
