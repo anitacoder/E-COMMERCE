ec9249831514b2124b72a3e6a646e7a6bf248671
Anita <writeanita.n@gmail.com>
2024-07-12 15:00:51 +0100

    styled the login form

--- myapp/src/Components/loginForm.jsx
+++ myapp/src/Components/loginForm.jsx
@@ -1,17 +1,24 @@
-import React from "react";
-import "./LoginForm.css"; 
+import React, { useState } from "react";
+import "./LoginForm.css";
 
 const LoginForm = () => {
+    const [isZoomed, setIsZoomed] = useState(false);
+
+    const handleZoom = () => {
+        setIsZoomed(true);
+        setTimeout(() => setIsZoomed(false), 600); // Remove the zoom class after the animation duration
+    };
+
     return (
-        <div className="login-container">
-            <h1>Login</h1>
+        <div className={`container ${isZoomed ? 'zoom' : ''}`} onClick={handleZoom}>
             <div className="login-content">
+                <h1>Login</h1>
                 <input type="email" placeholder="Email" className="login-input" />
                 <input type="password" placeholder="Password" className="login-input" />
                 <button className="login-button">Login</button>
-            </div>  
+            </div>
         </div>
-    )
+    );
 }
 
 export default LoginForm;
