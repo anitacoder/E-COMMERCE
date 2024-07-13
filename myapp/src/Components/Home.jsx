ef069d7d5a912c0d1e8640b156b25bc5aba3a19a
Anita <writeanita.n@gmail.com>
2024-07-13 09:32:28 +0100

    Added more skincare product and the description

--- myapp/src/Components/Home.jsx
+++ myapp/src/Components/Home.jsx
@@ -2,6 +2,8 @@ import React, {useRef} from "react";
 // import Slider from "react-slick";
 import "./Home.css";
 import faceWashImage from "./Images/simple_faceWash.png";
+import glycolicAcid from "./Images/glycolic_acid.png"
+import TheOrdinary  from "./Images/TheOrdinarySerum.png"
 
 const Home = () => {
     const productSectionRef = useRef(null);
@@ -47,22 +49,22 @@ const Home = () => {
                     <img src={faceWashImage} alt="Face Wash" className="faceWash" /> 
                     <p className="product-txt">Simple</p>
                     <h1 className="product-heading">Moisturising face wash</h1>
-                    <p className="product-t">For a glowing skin</p>
+                    <p className="product-t1">For a glowing skin</p>
                     <button className="add">Add to cart</button>
                     </div>
                     <div className="box2">
-                    <img src={faceWashImage} alt="Face Wash" className="faceWash" /> 
-                    <p className="product-txt">Simple</p>
-                    <h1 className="product-heading">Moisturising face wash</h1>
-                    <p className="product-t">For a glowing skin</p>
+                    <img src={glycolicAcid} alt="Acid" className="facewash" /> 
+                    <p className="product-txt1">Glycolic Acid</p>
+                    <h1 className="product-heading1">Exfloiating Treatment</h1>
+                    <p className="product-t">For a brighter complexion</p>
                     <button className="add">Add to cart</button>
                     </div>
                     <div className="box3">
-                    <img src={faceWashImage} alt="Face Wash" className="faceWash" /> 
-                    <p className="product-txt">Simple</p>
-                    <h1 className="product-heading">Moisturising face wash</h1>
-                    <p className="product-t">For a glowing skin</p>
-                    <button className="add">Add to cart</button>
+                    <img src={TheOrdinary} alt="Ordinary" className="facewash2" /> 
+                    <p className="product-txt2">The Ordinary</p>
+                    <h1 className="product-heading2">Hydrating Cleanser</h1>
+                    <p className="product-t2">For a balanced skin</p>
+                    <button className="add1">Add to cart</button>
                     </div>
                     <div className="box4">
                     <img src={faceWashImage} alt="Face Wash" className="faceWash" /> 
