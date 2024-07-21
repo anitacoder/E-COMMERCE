4dd75003ade377ad9a89ac447c189e4fc9f41509
Anita <writeanita.n@gmail.com>
2024-07-21 16:07:45 +0100

    Made the navbar responsive

--- myapp/src/Components/Navbar.jsx
+++ myapp/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchIcon from '@mui/icons-material/Search';
@@ -10,21 +10,18 @@ const Navbar = () => {
     const [data, setData] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
     const [searchWidth, setSearchWidth] = useState(150); 
-    const [isFocused, setIsFocused] = useState(false);
-    const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
-
+    const [isFocused, setIsFocused] = useState(false); 
+    
     const inputHandler = () => {
         setData(!data);
     };
 
     const menuHandler = () => {
         setMenuOpen(!menuOpen);
-    };
+    }
 
     const handleSearchWidth = (event) => {
-        if (isLargeScreen) {
-            setSearchWidth(event.target.value.length * 10);
-        }
+        setSearchWidth(event.target.value.length * 10); 
     };
 
     const handleFocus = () => {
@@ -35,65 +32,45 @@ const Navbar = () => {
         setIsFocused(false);
     };
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsLargeScreen(window.innerWidth > 768);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        // Cleanup listener on component unmount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
     return (
         <>
-            <div className="navbar_container">
-                <div className="navbar_div">
-                    <div>
-                        <Link to="/" className="link">Home</Link>
-                        <Link to="/about" className="link">About</Link>
-                        <Link to="/loginForm" className="link">Login</Link>
-                        <Link to="/signUp" className="link">SignUp</Link>
-                        <Link to="/contact" className="link">Contact</Link>
-                    </div>
-                    <div className={`search_container ${isFocused ? 'focused' : ''}`}>
-                        <SearchIcon className="search_icon_inside" style={{ right: `calc(10px + 100% - ${searchWidth}px)` }} />
-                        <input 
-                            type="text" 
-                            placeholder="Search here" 
-                            className="search" 
-                            onChange={handleSearchWidth} 
-                            onFocus={handleFocus} 
-                            onBlur={handleBlur} 
-                            style={{ width: isLargeScreen ? searchWidth : 'auto' }}
-                        />
-                        <CloseIcon onClick={inputHandler} className="close_icon" />
-                    </div>
-                    <div>
-                        <div className="navbar_main">
-                            <div></div>
-                            <div className="badSearch">
-                                <ShoppingCartIcon className="localMall_icon" />
-                                <MenuIcon onClick={menuHandler} className="menu_icon" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {menuOpen && (
-                    <div className="sidebar">
-                        <Link to="/" className="sidebar_link">Home</Link>
-                        <Link to="/about" className="sidebar_link">About</Link>
-                        <Link to="/loginForm" className="sidebar_link">Login</Link>
-                        <Link to="/signUp" className="sidebar_link">SignUp</Link>
-                        <Link to="/contact" className="sidebar_link">Contact</Link>
-                    </div>
-                )}
+        <div className="navbar_container">
+            <div className="navbar_div">
+                  <div>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/about" className="link">About</Link>
+                <Link to="loginForm" className="link">Login</Link>
+                <Link to="/signUp" className="link">SignUp</Link>
+                <Link to="/contact" className="link">Contact</Link>
             </div>
+            <div className={`search_container ${isFocused ? 'focused' : ''}`}>
+            <SearchIcon className="search_icon_inside" style={{ right: `calc(10px + 100% - ${searchWidth}px)` }} />
+            <input type="text" placeholder="Search here" className="search" onChange={handleSearchWidth} />
+                    <CloseIcon onClick={inputHandler} className="close_icon" />
+                </div>
+                <div>
+                    <div className="navbar_main">
+                        <div>
+                        </div>
+                      
+                        <div className="badSearch">
+                            <ShoppingCartIcon className="localMall_icon" />
+                            <MenuIcon onClick={menuHandler} className="menu_icon" />
+                        </div>
+                </div>
+            </div>
+            </div>
+            {menuOpen && (
+                <div className="sidebar">
+                    <Link to="/" className="sidebar_link">Home</Link>
+                    <Link to="/about" className="sidebar_link">About</Link>
+                    <Link to="loginForm" className="sidebar_link">Login</Link>
+                    <Link to="/signUp" className="sidebar_link">SignUp</Link>
+                    <Link to="/contact" className="sidebar_link">Contact</Link>
+                </div>
+            )}
+        </div>
         </>
     );
 };
-
 export default Navbar;
